6b38020dccfdd86f32970fd7cc398f8cc563067b
Jocemar <jocemarvosskovacs@gmail.com>
2022-06-01 21:54:59 -0300

    Validação do Formulário com JavaScript

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -4,6 +4,8 @@ const email = document.getElementById('email')
 const telefone = document.getElementById('telefone')
 const empresa = document.getElementById('empresa')
 
+const textBtn = document.getElementsByClassName('form-submit')[0]
+
 //quando apertar o submit não recarregar a pagina
 form.addEventListener("submit", (e) => {
     e.preventDefault();
@@ -20,6 +22,7 @@ function checkInputs(){
     //Validando nome
     if(nomeValor === ''){
         setErrorFor(nome, "O campo nome é obrigatório!")
+        textBtn.innerText = 'Quero Produzir mais!'
     } else {
         setSuccessFor(nome);
     }
@@ -27,8 +30,10 @@ function checkInputs(){
     //validando email
     if(emailValor === ''){
         setErrorFor(email, "Por favor preencha seu e-mail");
+        textBtn.innerText = 'Quero Produzir mais!'
     } else if(!checkEmail(emailValor)) {
         setErrorFor(email, "E-mail inválido!");
+        textBtn.innerText = 'Quero Produzir mais!'
     } else {
         setSuccessFor(email);
     }
@@ -36,6 +41,7 @@ function checkInputs(){
     //validando telefone
     if((telefoneValor === '') || (telefoneValor.length < 10)){
         setErrorFor(telefone, "Por favor preencha seu telefone");
+        textBtn.innerText = 'Quero Produzir mais!'
     } else {
         setSuccessFor(telefone)
     }
@@ -43,8 +49,25 @@ function checkInputs(){
     //Validando empresa
     if((empresaValor === '') || (empresaValor.length < 4)){
         setErrorFor(empresa, "Por favor informe sua empresa")
+        textBtn.innerText = 'Quero Produzir mais!'
     } else {
         setSuccessFor(empresa);
+    }
+
+    const formDivs = document.querySelectorAll('.div-form')
+    const formIsValid = [...formDivs].every(formDiv => {
+        return formDiv.className === 'div-form success'
+    });
+
+    if(formIsValid){        
+        
+        textBtn.innerText = 'Dados Enviados!'
+        inputs = [...form.querySelectorAll('input')]
+
+        for (i in inputs){
+            console.log(inputs[i].value='');
+        }
+        
     }
 }
 
